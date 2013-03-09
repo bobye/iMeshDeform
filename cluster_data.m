300b639422ccabad1525e75f8265d34435529b12
Jianbo Ye <yelpoo@gmail.com>
2013-03-09 19:22:56 +0800

    revise rotation groups export

--- cluster_data.m
+++ cluster_data.m
@@ -1,14 +1,16 @@
 function cluster_data( fileprefix, number_of_cluster )
 tic;
+v2c = load([fileprefix '.id']); v2c = -v2c;
 fid = fopen([fileprefix '_examples.embd']);
 n = fscanf(fid, '%d',1); d = fscanf(fid,'%d',1);
 R = fscanf(fid, '%f');
 R = reshape(R,d,n);
+R = R(:,v2c == 0);
 IDX = kmeans(R', number_of_cluster);
-dlmwrite([fileprefix, '_cluster.id'], IDX-1);
+v2c(v2c == 0) = IDX - 1;
+dlmwrite([fileprefix, '_cluster.id'], v2c);
 dlmwrite([fileprefix, '_cluster.color'],hsv(number_of_cluster),'delimiter', '\t');
 fclose(fid);
 toc;
-
 
 
