7767dcf73bb58fcc700b5a31686100a88eda9a1a
Jianbo Ye <yelpoo@gmail.com>
2013-03-29 18:59:47 +0800

    modified:   run.sh 	new file:   test2.1.rotation.id

--- example/test3/run.sh
+++ example/test3/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-../../bin/iTetDeform test2.1.node test2.1.cluster25.id test2.1.cluster25.id test2.1.rigid.id -pc_factor_mat_solver_package umfpack
+../../bin/iTetDeform test2.1.node test2.1.cluster25.id test2.1.rotation.id test2.1.rigid.id -pc_factor_mat_solver_package umfpack
