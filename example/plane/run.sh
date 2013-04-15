41c168b2c8202e5f45de9e04a9ce12a76edfccc0
Zhixin Yan <yzxsunshine@gmail.com>
2013-04-15 18:18:28 +0800

    Add plane_v cluster file

--- example/plane/run.sh
+++ example/plane/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # run main program
-../../bin/iMeshDeform plane.off plane_linear25_cluster.id plane_rot25_cluster.id plane_affine.id -pc_factor_mat_solver_package umfpack
+../../bin/iMeshDeform plane_v.off plane_v_cluster27.id plane_v_rotation25.id plane_v_affine.id -pc_factor_mat_solver_package umfpack
 
