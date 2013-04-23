26ecdc123e4b222c3d0408e89ab9eec2805cefad
Zhixin Yan <yzxsunshine@gmail.com>
2013-04-23 20:59:18 +0800

    Add teapot trimesh and tetmesh pose, rotation group, affine group, rigid constraints, and the final mesh

---
+++ example/teapot/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# run main program
+../../bin/iMeshDeform teapot.off cluster28.id rotation25.id affine.id -pc_factor_mat_solver_package umfpack
+
---
+++ example/teapottet/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# run main program
+../../bin/iTetDeform teapot.1.node cluster28.id rotation25.id affine.id -pc_factor_mat_solver_package umfpack
+
