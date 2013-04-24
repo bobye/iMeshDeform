b98d9211da93c78419f765a6874ae498b9c0adf6
Jianbo Ye <yelpoo@gmail.com>
2013-04-24 09:43:12 +0800

    new file:   example/dinosaur/dinosaur_lin34.id 	modified:   example/dinosaur/run.sh

--- example/dinosaur/run.sh
+++ example/dinosaur/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # run main program
-../../bin/iMeshDeform dinosaur.off dinosaur_cluster25.id dinosaur_cluster25.id dinosaur_empty.id -pc_factor_mat_solver_package umfpack
+../../bin/iMeshDeform dinosaur.off dinosaur_lin34.id dinosaur_cluster30.id dinosaur_affine.id -pc_factor_mat_solver_package umfpack
 
