e9d84a8c6f381f1973c0a3fdf222281884422009
Zhixin Yan <yzxsunshine@gmail.com>
2013-04-26 20:34:26 +0800

    box model

---
+++ example/box/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# run main program
+../../bin/iMeshDeform box.off cluster25.id rotation25.id affine.id -pc_factor_mat_solver_package umfpack
+
---
+++ example/boxtet/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# run main program
+../../bin/iTetDeform box.1.node cluster25.id rotation25.id affine.id -pc_factor_mat_solver_package umfpack
+
