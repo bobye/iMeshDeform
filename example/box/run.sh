d20ff5a43548ab9c8f737bb07b7327f2808ced16
Jianbo Ye <yelpoo@gmail.com>
2013-04-27 16:54:40 +0800

    revise box model

--- example/box/run.sh
+++ example/box/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # run main program
-../../bin/iMeshDeform box.off cluster25.id rotation25.id affine.id -pc_factor_mat_solver_package umfpack
-
+#../../bin/iMeshDeform box.off cluster25.id cluster25.id affine.id -pc_factor_mat_solver_package umfpack
+#../../bin/iMeshDeform box2.2.off box2.2.cluster25.id box2.2.cluster25.id box2.2.empty.id -pc_factor_mat_solver_package umfpack
--- example/boxtet/run.sh
+++ example/boxtet/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # run main program
-../../bin/iTetDeform box.1.node cluster25.id rotation25.id affine.id -pc_factor_mat_solver_package umfpack
+../../bin/iTetDeform box.node cluster25.id cluster25.id affine.id -pc_factor_mat_solver_package mumps
 
